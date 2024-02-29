38c57e0e613f707f363695eeccf50d3eb7404969
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-02-28 21:24:01 -0500

    add draw context attributes and setup window, input & cleanup

--- function_glGetString/main.cpp
+++ function_glGetString/main.cpp
@@ -1,23 +1,83 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
-void InitializeProgram(){
-  if(SDL_Init(SDL_INIT_VIDEO) < 0){
+// Globals
+int gScreenWidth = 640;
+int gScreenHeight = 480;
+
+SDL_Window* gGraphicsApplicationWindow = nullptr;
+SDL_GLContext gOpenGLContext = nullptr;
+
+bool gQuit = false;
+
+void InitializeProgram() {
+  if(SDL_Init(SDL_INIT_VIDEO) < 0) {
     std::cout << "SDL2 could not initialize video subsystem"
               << std::endl;
     exit(1);
   }
+
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+  gGraphicsApplicationWindow = SDL_CreateWindow("OpenGL SDL2 Demo - Window Test",
+                                10, 35, 
+                                gScreenWidth, gScreenHeight, 
+                                SDL_WINDOW_OPENGL);
+
+  if (gGraphicsApplicationWindow == nullptr) {
+    std::cout << "SDL_Window was not able to be created" << std::endl;
+    exit(1);
+  }
+
+  gOpenGLContext = SDL_GL_CreateContext(gGraphicsApplicationWindow);
+
+  if (gOpenGLContext == nullptr) {
+    std::cout << "OpenGL context not available\n";
+    exit(1);
+  }
 }
 
-void MainLoop(){
+void Input() {
+  SDL_Event e;
+
+  while (SDL_PollEvent(&e) != 0) {
+    if (e.type == SDL_QUIT) {
+      std::cout << "Goodbye!" << std::endl;
+      gQuit = true;
+    }
+  }
+}
+
+void PreDraw() {
 
 }
 
-void CleanUp(){
+void Draw() {
+
+}
+
+void MainLoop() {
+  while(!gQuit) {
+    Input();
+    PreDraw();
+    Draw();
+
+    // Update Screen
+    SDL_GL_SwapWindow(gGraphicsApplicationWindow);
+  }
+}
+
+void CleanUp() {
+  SDL_DestroyWindow(gGraphicsApplicationWindow);
   SDL_Quit();
 }
 
-int main(int, char **){
+int main(int, char **) {
   InitializeProgram();
   MainLoop();
   CleanUp();
